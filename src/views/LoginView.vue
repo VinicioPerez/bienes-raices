f6d09c14f958001203ab0004fd4bf47e75ae2b0f
Marco Perez <108315533+VinicioPerez@users.noreply.github.com>
2024-08-05 15:07:10 -0500

    agregando el formulario de inicio de sesion con las clase de vutify

---
+++ src/views/LoginView.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+</script>
+
+<template>
+    
+    <v-card
+        flat 
+        max-width="500"
+        class="mx-auto my-10"
+    >
+        <v-card-title 
+            class="text-h3 text-center"
+            tag="h3"
+        >
+            Iniciar Sesión
+        </v-card-title>
+        <v-card-subtitle 
+            class="text-h5 text-center"
+        >
+            Inicia Sesión con tu cuenta
+        </v-card-subtitle>
+        <v-form class="mt-5">
+            <v-text-field
+                type="email"
+                label="Email"
+                color="blue-darken-1" 
+                clearable              
+                bg-color="blue-grey-lighten-5"
+            />
+            <v-text-field
+                type="password"
+                label="Password"
+                color="blue-darken-1"               
+                bg-color="blue-grey-lighten-5"
+            />
+            <v-btn
+                block
+                color="blue-darken-2"
+            >
+                Iniciar Sesión
+            </v-btn>
+
+            <!-- <v-otp-input></v-otp-input> -->
+
+            
+        </v-form>
+    </v-card>
+</template>
