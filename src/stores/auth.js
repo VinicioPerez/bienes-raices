be7367fcdb25f7aa8fcae525761eea347ef5f1dd
Marco Perez <108315533+VinicioPerez@users.noreply.github.com>
2024-08-06 09:59:32 -0500

    protegiendo los distintos archovos para cuando si un usuarios esta autenticado permitirle el ingreso

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,15 +1,16 @@
 import { ref, computed, onMounted} from 'vue';
 import { defineStore } from "pinia";
 import { useFirebaseAuth } from 'vuefire';
-import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+import { useRouter } from 'vue-router';
 
 export const useAuthStore = defineStore('auth', () => {
 
     const auth = useFirebaseAuth()
     const authUser = ref(null)
+    const router = useRouter()
 
-    console.log(authUser.value)
-
+   
     const errorMsg = ref('')
 
     const errorCodes = {
@@ -30,13 +31,23 @@ export const useAuthStore = defineStore('auth', () => {
             .then((userCredential) => {
                 const user = userCredential.user
                 authUser.value = user
-
-                console.log(authUser.value)
+                router.push({name: 'admin-propiedades'})
+                // console.log(authUser.value)
         })
         .catch(error => {
             console.log(errorCodes[error.code])
             errorMsg.value = errorCodes[error.code]
         });
+    }
+
+    const logout = () => {
+        signOut(auth).then(() => {
+            authUser.value = null
+            router.push({name: 'login'})
+        }).catch( error => {
+            console.log(error)
+        })
+
     }
 
     const hasError = computed(() => {
@@ -54,6 +65,7 @@ export const useAuthStore = defineStore('auth', () => {
 
     return {
         login,
+        logout,
         hasError,
         errorMsg,
         isAuth,
--- src/views/admin/AdminLayout.vue
+++ src/views/admin/AdminLayout.vue
@@ -1,1 +1,7 @@
-te
+<script setup>
+
+</script>
+
+<template>
+ <h2 class="text-center text-h3 my-5 font-weight-bold">Admin Panel</h2>
+</template>
